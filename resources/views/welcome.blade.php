e43a4f0df2cd66830bf959c6cfda52d96074a8d5
Matteogatto94 <cosenza94@live.it>
2022-12-16 16:48:43 +0100

    add database with data and complete section comic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,21 @@
 <div class="container">
     <button type="button" class="btn btn-primary btn-lg">CURRENT SERIES</button>
 
-    <div class="row">
-        <div class="col">
+    <div class="row row-cols-md-6 g-4 mt-4 text-center">
 
+        @forelse($comics as $comic)
+        <div class="col">
+            <img class="card-img-top" src="{{ $comic['thumb'] }}">
+            <p>{{$comic['series']}}</p>
         </div>
+        @empty
+        <div class="col">
+            <p>No comics in this show yet.</p>
+        </div>
+        @endforelse
     </div>
-    <button type="button" class="btn btn-primary">LOAD MORE</button>
+    <div class="button_bottom d-flex justify-content-center">
+        <button type="button" class="btn btn-primary">LOAD MORE</button>
+    </div>
 </div>
 @endsection
