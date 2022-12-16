22203a1807d8cf3d6582bba6ff087b74dde866c2
Matteogatto94 <cosenza94@live.it>
2022-12-16 17:20:00 +0100

    add banner list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,4 +22,35 @@
         <button type="button" class="btn btn-primary">LOAD MORE</button>
     </div>
 </div>
+
+<section class="banner_list bg-primary">
+    <div class="container text-white text-center">
+        <div class="row">
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                <span>DIGITAL COMICS</span>
+            </div>
+
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                <span>DC MERCHANDISE</span>
+            </div>
+
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                <span>SUBSCRIPTION</span>
+            </div>
+
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="" class="locator">
+                <span>COMIC SHOP LOCATOR</span>
+            </div>
+
+            <div class="col">
+                <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                <span>DC POWER VISA</span>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
