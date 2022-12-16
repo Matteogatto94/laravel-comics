e2410c3037369d68274a7e41db9d7bef2046f34c
Matteogatto94 <cosenza94@live.it>
2022-12-16 16:24:01 +0100

    add jumbotron

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Welcome</h1>
+<div class="jumbotron"></div>
+<div class="container">
+    <button type="button" class="btn btn-primary btn-lg">CURRENT SERIES</button>
+
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
+    <button type="button" class="btn btn-primary">LOAD MORE</button>
+</div>
 @endsection
